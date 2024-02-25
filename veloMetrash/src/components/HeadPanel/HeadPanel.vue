0f8b6744b5f03600f4f919c714e82345e86869f0
Gubaev Vadim <vadim.gubaev@mail.ru>
2024-02-26 00:38:58 +0500

    Начальный коммит

---
+++ veloMetrash/src/components/HeadPanel/HeadPanel.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="header">
+    <h1 class="header-title">Спортив</h1>
+  </div>      
+</template>
+
+<style lang="scss" scoped>
+.header {
+  background-color: #FFFFFF;
+  height: 50px;
+  display: flex;
+  align-items: center;
+}
+.header-title {
+  color: #151617;
+  margin-left: 20px;
+  font-size: 1.5rem;
+  font-family: Helvetica, Arial, sans-serif;
+  font-weight:600;
+}
+</style>
---
+++ veloMetrash/src/components/NavMenu/NavMenu.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="navbar">
+        <h1 class="navbar_title">Участники</h1>
+        <h1 class="navbar_title">Протокол</h1>
+        <h1 class="navbar_title">Промежуточные результаты</h1>
+        <h1 class="navbar_title">Просмотр</h1>
+        <h1 class="navbar_title">FAQ</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+
+<style scoped>
+.navbar {
+    display:flex;
+    flex-direction: column;
+    gap:5px;
+    width:300px;
+}
+.navbar_title {
+    color: #000000;
+    margin-left: 20px;
+    font-size: 1rem;
+    font-family: Helvetica, Arial, sans-serif;
+
+}
+
+
+</style>
